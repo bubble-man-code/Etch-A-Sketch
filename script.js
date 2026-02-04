fa2a51ecb4e3df97b46d19c9ddf8298edb9cf613
bubble-man-code <181590881+bubble-man-code@users.noreply.github.com>
2026-02-04 12:29:05 +0100

    set up hover effect

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 for (let i = 0; i < 256; i++) {
 const div = document.createElement('div');
 div.classList.add('box');
-div.textContent = i;
+div.textContent = "";
 document.getElementById('container').appendChild(div);
-}
+div.addEventListener('mouseenter', function(e) {
+    e.target.style.backgroundColor = 'black';
+})
+div.addEventListener('mouseleave', function(e) {
+    e.target.style.backgroundColor = 'lightblue';
+})}
