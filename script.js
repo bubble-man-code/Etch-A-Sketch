d6649bb7d7de4b0aa0b753cd8b8fa2bf0eed06d8
bubble-man-code <181590881+bubble-man-code@users.noreply.github.com>
2026-02-06 19:34:24 +0100

    finish resize function and add button

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-for (let i = 0; i < 256; i++) {
+let numberOfBoxes = 16;
+var r = document.querySelector(':root');
+function myFunction_set() {
+  r.style.setProperty('--n', numberOfBoxes);
+}
+let createBoxes = function() {
+for (let i = 0; i < (numberOfBoxes * numberOfBoxes); i++) {
 const div = document.createElement('div');
 div.classList.add('box');
 div.textContent = "";
@@ -8,4 +14,15 @@ div.addEventListener('mouseenter', function(e) {
 })
 div.addEventListener('mouseleave', function(e) {
     e.target.style.backgroundColor = 'lightblue';
-})}
+})}}
+createBoxes()
+const resizeButton = document.getElementById('resizeButton');
+resizeButton.addEventListener('click', function() {
+    const container = document.getElementById('container');
+container.innerHTML = ''; // Wipes everything inside
+    let newSize = prompt("Enter new grid size (max 100):");
+    newSize = Math.min(newSize, 100);
+numberOfBoxes = newSize;
+myFunction_set(); 
+createBoxes()
+   })
